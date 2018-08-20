7d8179f6eae56508e4d95899565382344e323277
Lasse Tyrihjell <lassetyr@gmail.com>
2018-08-20 10:07:25 +0200

    Reduce logging

--- src/main/java/org/entur/namtar/routes/api/MappingRoute.java
+++ src/main/java/org/entur/namtar/routes/api/MappingRoute.java
@@ -73,11 +73,9 @@ public class MappingRoute extends RestRouteBuilder {
         from("direct:createResponse")
             .choice()
                 .when(body().isNull())
-                    .log("Body is null")
                     .setHeader(Exchange.HTTP_RESPONSE_CODE, constant("404"))
                 .otherwise()
-                .log("Body is [${body}]")
-                .bean(mapper, "writeValueAsString(${body})")
+                    .bean(mapper, "writeValueAsString(${body})")
             .endChoice()
             .routeId("namtar.createResponse")
         ;
