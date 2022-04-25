c05c6405122c07be07f6ad7d9faf64e7a896c5d5
Lasse Tyrihjell <lassetyr@gmail.com>
2022-04-25 10:38:56 +0200

    Revert "Updating terrafrom versions"
    This reverts commit fad0b2db751e1e3fc8e864e0d3e2d53c34e98a0d.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,13 @@
 # Contains main description of bulk of terraform?
 terraform {
-  required_version = ">= 0.13.2"
+  required_version = ">= 0.12"
 }
 
 provider "google" {
-  version = "~> 3.74.0"
+  version = "~> 2.19"
 }
 provider "kubernetes" {
-  version = "~> 1.13.4"
+  version = "~> 1.13.3"
   load_config_file = var.load_config_file
 }
 
